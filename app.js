fed466789036e283b428e345b579c8cd9319a6e8
Jukka Prami <43521@edu.raseko.fi>
2023-08-29 15:21:48 +0300

    1

--- app.js
+++ app.js
@@ -27,7 +27,11 @@ app.set(`view engine`, `handlebars`);
 // URL ROUTES
 // -----------
 
+app.get(`/`, (req, res) => {
+    res.render(`index`)
+});
+
 // START THE LISTENER
-app.listen(PORT)
-console.log(`Server started and it will listen TCP port`, PORT)
+app.listen(PORT);
+console.log(`Server started and it will listen TCP port`, PORT);
 
