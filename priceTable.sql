2e32b0a10fa940489ee28964e8946d001794ef67
Jukka Prami <43521@edu.raseko.fi>
2023-09-07 15:58:11 +0300

    Create priceTable.sql
    sql Table

---
+++ priceTable.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE public.price (
+                start TIMESTAMP NOT NULL,
+                end_1 TIMESTAMP NOT NULL,
+                price REAL NOT NULL,
+                CONSTRAINT price_pk PRIMARY KEY (start, end_1)
+);
+COMMENT ON TABLE public.price IS 'Stores electricity hourly prices and timestamps';
+COMMENT ON COLUMN public.price.start IS 'Date and hour when time period starts ';
+COMMENT ON COLUMN public.price.end_1 IS 'When time period ends';
+COMMENT ON COLUMN public.price.price IS 'Hourly price cents';
