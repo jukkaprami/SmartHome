fe22dec6d6f5f02aebadf6e9d3403a1ff039f2ff
Jukka Prami <43521@edu.raseko.fi>
2023-09-07 13:42:57 +0300

    xxx

--- app.js
+++ app.js
@@ -98,40 +98,6 @@ app.get('/test',(req, res) => {
 
 });
 
-const LATEST_PRICES_ENDPOINT = 'https://api.porssisahko.net/v1/latest-prices.json';
-
-async function fetchLatestPriceData() {
-  const response = await fetch(LATEST_PRICES_ENDPOINT);
-
-  return response.json();
-}
-
-function getPriceForDate(date, prices) {
-  const matchingPriceEntry = prices.find(
-    (price) => new Date(price.startDate) <= date && new Date(price.endDate) > date
-  );
-
-  if (!matchingPriceEntry) {
-    throw 'Price for the requested date is missing';
-  }
-
-  return matchingPriceEntry.price;
-}
-
-// Note that it's enough to call fetchLatestPriceData() once in 12 hours
-const { prices } = await fetchLatestPriceData();
-
-try {
-  const now = new Date();
-  const price = getPriceForDate(now, prices);
-
-  console.log(`Hinta nyt (${now.toISOString()}): ${price} snt / kWh (sis. alv)`);
-} catch (e) {
-  console.error(`Hinnan haku epäonnistui, syy: ${e}`);
-}
-
-
-
 // START THE LISTENER
 app.listen(PORT);
 console.log('Server started and it will listen TCP port', PORT);
