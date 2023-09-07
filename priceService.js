512a1d8b55a1e9870fe17d2489a39a877340a647
Jukka Prami <43521@edu.raseko.fi>
2023-09-07 14:16:12 +0300

    xx

--- priceService.js
+++ priceService.js
@@ -2,28 +2,28 @@
 const LATEST_PRICES_ENDPOINT = 'https://api.porssisahko.net/v1/latest-prices.json';
 
 /* Create a promise for reading data from the API in the background
-and return it as json when ready */
+ and return it as json when ready */
 async function fetchLatestPriceData() {
-  const response = await fetch(LATEST_PRICES_ENDPOINT);
-  const json = await response.json()
-  return json;
+    const response = await fetch(LATEST_PRICES_ENDPOINT);
+    const json = await response.json()
+    return json;
 }
 
 // Get price for a given one hour inteval
 function getPriceForDate(date, prices) {
-  const matchingPriceEntry = prices.find(
-    (price) => new Date(price.startDate) <= date && new Date(price.endDate) > date
-  );
+    const matchingPriceEntry = prices.find(
+        (price) => new Date(price.startDate) <= date && new Date(price.endDate) > date
+    );
 
-  // if not found rise and error
-  if (!matchingPriceEntry) {
-    throw 'Price for the requested date is missing';
-  }
+    // If not found raise an error
+    if (!matchingPriceEntry) {
+        throw 'Price for the requested date is missing';
+    }
 
-  return matchingPriceEntry.price;
+    return matchingPriceEntry.price;
 }
 
-// When promise has been fullfilled
+// When promise has been fullfilled log results or the error
 fetchLatestPriceData()
     .then((json) => console.log(json))
     .catch((err) => console.log(err))
