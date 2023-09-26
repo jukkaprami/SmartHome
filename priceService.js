1f01f11ff9fa0989bd783bed085ffd9ce7b4d97e
Jukka Prami <43521@edu.raseko.fi>
2023-09-26 10:45:45 +0300

    uudet

--- priceService.js
+++ priceService.js
@@ -1,4 +1,4 @@
-// Set the URL for API endpoint: latest prices at 14.00 EET
+// Set the URL for API endpoint: latest prices at 14.00 EET or 15.00 at summer
 const LATEST_PRICES_ENDPOINT = 'https://api.porssisahko.net/v1/latest-prices.json';
 
 /* Create a promise for reading data from the API in the background
