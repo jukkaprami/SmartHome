29ec08b3f2ab12a1203c9b74861acd74b6df8574
Jukka Prami <43521@edu.raseko.fi>
2023-10-03 10:05:44 +0300

    uusi tiedosto

--- logger.js
+++ logger.js
@@ -1,4 +1,18 @@
+const fs = require('fs');
 
+const timestamp = new Date();
+const isoTimeStamp = timestamp.toISOString();
+let operatio = 'Data Fetch operation';
+let status = 'processed succesfully';
+let entry = operation + ' ' +status + ' ' + isoTimeStamp + '\n';
+
+console.log(entry);
+
+fs.appendFile('.dataOperations.log', entry, (err) => {
+    if (err) {
+        console.log(err)
+    }
+})
 
 
 
