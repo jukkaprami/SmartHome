f223b9fac8823cc4a4841297531365b249ff098a
Jukka Prami <43521@edu.raseko.fi>
2023-11-02 09:57:36 +0200

    09

--- weatherGet.js
+++ weatherGet.js
@@ -1,13 +1,29 @@
-const getData = async () => {
-    let city = document.getElementById("Turku").value; //input from user.
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`;
-    try {
-      let res = await fetch(url);
-      let data = await res.json();
-      let lat = data.coord.lat;
-      let lon = data.coord.lon;
-      getDatafor7days(lat, lon);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+var SERVER_URL = "http://opendata.fmi.fi/wfs";
+var STORED_QUERY_OBSERVATION = "fmi::observations::weather::multipointcoverage";
+const MetOClient = require('@fmidev/metoclient');
+
+const metoclient = new MetOClient(options);
+metoclient.render().then(function (map) {
+  metoclient.play({
+    delay: 1000,
+    time: Date.now()
+  });
+}).catch(err => {
+  // statements to handle any exceptions
+});
+var connection = new fi.fmi.metoclient.WfsConnection();
+if (connection.connect(SERVER_URL, STORED_QUERY_OBSERVATION)) {
+    // Connection was properly initialized. So, get the data.
+    connection.getData({
+        requestParameter : "td",
+        begin : new Date(1368172800000),
+        end : new Date(1368352800000),
+        timestep : 60 * 60 * 1000,
+        sites : "Helsinki",
+        callback : function(data, errors) {
+            // Handle the data and errors object in a way you choose.
+            handleCallback(data, errors);
+            // Disconnect because the flow has finished.
+            connection.disconnect();
+        }
+    });
