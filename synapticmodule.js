0a176217d20e4ed0bcfe80a298324db257fa6f0f
Jukka Prami <43521@edu.raseko.fi>
2024-02-03 20:36:56 +0200

    modulin alku 3.2.2024

---
+++ synapticmodule.js
@@ -0,0 +1,10 @@
+const synaptic = require('synaptic');
+const neuralNetwork = synaptic.Network;
+
+class SynapticModule {
+    constructor() {
+        this.neuralNetwork = synaptic.Network;
+    }
+}
+
+module.exports = SynapticModule;
